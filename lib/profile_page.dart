55e4ec33395879b24d1ac6c6e5b8d5154c342571
dhrudip <dhrudiptimbaliya535@gmail.com>
2026-05-21 14:43:10 +0530

    Initial commit

--- lib/profile_page.dart
+++ lib/profile_page.dart
@@ -39,7 +39,7 @@ class ProfilePage extends StatelessWidget {
                 borderRadius: BorderRadius.circular(20),
                 boxShadow: [
                   BoxShadow(
-                    color: Colors.blue.withOpacity(0.1),
+                    color: Colors.blue.withValues(alpha: 0.1),
                     blurRadius: 20,
                     offset: const Offset(0, 10),
                   ),
